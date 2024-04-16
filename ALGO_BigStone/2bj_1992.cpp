a215e4b28763507d88f0a396b7facaf8442ee5ea
mac <mac@macui-noteubug.local>
2024-04-16 15:08:05 +0900

    22D& C...

--- ALGO_BigStone/2bj_1992.cpp
+++ ALGO_BigStone/2bj_1992.cpp
@@ -2,45 +2,28 @@
 
 using namespace std;
 
-int dx[]={-1,0,1,0};
-int dy[]={0,1,0,-1};
-int matrix[65][65]={0,};
-int visited[65][65]={0,};
+char matrix[101][101]={0,};
 
 int N;
 
-void dfsQuadTreeZip(int y, int x, int dst_y, int dst_x, int sum, int cnt){
-    visited[y][x]=1;
-    cnt+=1;
-    sum+=matrix[y][x];
-    // cout<<"y: "<<y<<", x: "<<x<<"\tval:"<<matrix[y][x]<<endl;
-    // cout<<"dst_y: "<<dst_y<<", dst_x: "<<dst_x<<"\tval:"<<matrix[y][x]<<endl;
-    if((cnt==(dst_y-y)*(dst_x-x))&&(sum==cnt||sum==0)){
-        cout<<matrix[y][x];
-    }else if((cnt==(dst_y-y)*(dst_x-x))&&(sum!=cnt&&sum!=0)){
-        for(int k=0; k<4; k++){
-            int nx = x+dx[k];
-            int ny = y+dy[k];
-            if(nx<0||nx>dst_x-1||ny<0||ny>dst_y-1) continue;
-            if((!visited[ny][nx])){
-                sum = cnt = 0;
-                cout<<"(";
-                dfsQuadTreeZip(ny, nx, dst_y/2, dst_x/2, sum, cnt); // lu
-                dfsQuadTreeZip(ny, dst_x/2, dst_y/2, dst_x, sum, cnt); // ru
-                dfsQuadTreeZip(dst_y/2, nx, dst_y, dst_x/2, sum, cnt); // ld
-                dfsQuadTreeZip(dst_y/2, dst_x/2, dst_y, dst_x, sum, cnt); // rd
-                cout<<")";
+string QuadTreeZip(int y, int x, int size){
+    if(size==1) return string(1,matrix[y][x]);
+    char check = matrix[y][x];
+    string ss="";
+    for(int i=y; i<y+size; i++){
+        for(int j=x; j<x+size; j++){
+            if(matrix[i][j]!=check){
+                ss.append("(");
+                ss.append(QuadTreeZip(y,x,size/2)); // lu
+                ss.append(QuadTreeZip(y,x+size/2,size/2)); // ru
+                ss.append(QuadTreeZip(y+size/2,x,size/2)); // ld
+                ss.append(QuadTreeZip(y+size/2,x+size/2,size/2)); // rd
+                ss.append(")");
+                return ss;
             }
         }
-    }else{
-        for(int k=0; k<4; k++){
-            int nx = x+dx[k];
-            int ny = y+dy[k];
-            if(nx<0||nx>dst_x-1||ny<0||ny>dst_y-1) continue;
-            if((!visited[ny][nx])) dfsQuadTreeZip(ny, nx, dst_y, dst_x, sum, cnt);
-        }
     }
-
+    return string(1,matrix[y][x]);
 }
 
 // 재귀로 주어진 범위 내 값이 다 동일하면 압축. 아니면 재귀로 Quad
@@ -59,10 +42,10 @@ int main(){
     cin>>N;
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
-            scanf("%1d", &matrix[i][j]);
+            scanf("%1s", &matrix[i][j]);
         }
     }
-    dfsQuadTreeZip(0,0,N,N,0,0);
+    cout<<QuadTreeZip(0,0,N)<<endl;
 
     return 0;
 }
