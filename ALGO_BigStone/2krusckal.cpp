6f0a652325a5a6481f829baad21258f04b96856e
mac <mac@macui-noteubug.local>
2024-04-20 17:17:52 +0900

    const -> overloading operator

--- ALGO_BigStone/2krusckal.cpp
+++ ALGO_BigStone/2krusckal.cpp
@@ -2,11 +2,7 @@
 
 using namespace std;
 
-typedef pair <int, pair<int,int>> p;
-
-vector<p> edges;
 int vertices[100001];
-int v=6;
 
 int getRoot(int x){
     if(vertices[x]==x) return x;
@@ -24,46 +20,60 @@ int isSameParent(int a, int b){
     if(a==b) return 1;
     else return 0;
 }
-vector<p> kruskal(){
-    vector<p> mst;
+class Edge {
+public:
+    int u, v, weight;
+    Edge(int u, int v, int weight) : u(u), v(v), weight(weight) {}
+
+    bool operator < (const Edge& edge) const {
+        return weight < edge.weight;
+    }
+};
+
+int n = 7, m = 11;
+int sum = 0;
+vector<Edge> edges;
+
+void init(){
+    edges.push_back(Edge(1,7,12));
+    edges.push_back(Edge(1,4,28));
+    edges.push_back(Edge(1,2,67));
+    edges.push_back(Edge(1,5,17));
+    edges.push_back(Edge(2,4,24));
+    edges.push_back(Edge(2,5,62));
+    edges.push_back(Edge(3,5,20));
+    edges.push_back(Edge(3,6,37));
+    edges.push_back(Edge(4,6,13));
+    edges.push_back(Edge(5,6,45));
+    edges.push_back(Edge(5,7,73));
+
+    sort(edges.begin(), edges.end());
+
+    for (int i = 0; i < n; i++) vertices[i] = i;
+}
+vector<Edge> kruskal(){
+    vector<Edge> mst;
     for(int i=0; i<edges.size(); i++){
-        p cur_edge = edges[i];
+        if(!isSameParent(edges[i].u-1, edges[i].v-1)){
+            sum+=edges[i].weight;
+            mst.push_back(edges[i]);
+            unionParent(edges[i].u-1, edges[i].v-1);
+        }
 
-        int f = cur_edge.second.first;
-        int s = cur_edge.second.second;
-
-        if(getRoot(f)==getRoot(s)) continue;
-        mst.push_back(cur_edge);
-        unionParent(f,s);
-
-        if (mst.size() == v - 1) return mst;
+        if(mst.size()==n-1) return mst;
     }
     return mst;
 }
 
-void init(){
-    edges.push_back({ 1, {1, 5} });
-    edges.push_back({ 4, {1, 3} });
-    edges.push_back({ 3, {1, 4} });
-    edges.push_back({ 9, {1, 2} });
-    edges.push_back({ 4, {2, 4} });
-    edges.push_back({ 5, {2, 5} });
-    edges.push_back({ 6, {3, 6} });
-    edges.push_back({ 2, {4, 5} });
-    edges.push_back({ 8, {4, 6} });
-
-    sort(edges.begin(), edges.end());
-
-    for (int i = 1; i <= 6; i++) vertices[i] = i;
-}
 int main(){
     init();
-    vector<p> mst = kruskal();
+    vector<Edge> mst = kruskal();
 
     cout<<"[MST :: KRUSKAL]\n";
-    for (int i = 0; i < mst.size(); i++) {
-        cout<<mst[i].second.first<<", "<< mst[i].second.second<<", dist: "<< mst[i].first<<"\n";
+    for(int i=0; i<mst.size(); i++){
+        cout<<mst[i].u<<" - "<<mst[i].v<<" : cost = "<<mst[i].weight<<endl;
     }
+    cout<<"total sum: "<<sum<<endl;
 
     return 0;
 }
