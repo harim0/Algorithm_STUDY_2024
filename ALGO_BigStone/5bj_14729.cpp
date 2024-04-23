370b401e5c4032ab1a97c1bca8960ac776dfbb2a
mac <mac@macui-noteubug.local>
2024-04-23 16:49:57 +0900

    E.E

--- ALGO_BigStone/5bj_14729.cpp
+++ ALGO_BigStone/5bj_14729.cpp
@@ -3,22 +3,28 @@
 using namespace std;
 
 int n;
-priority_queue<double, vector<double>, greater<double>> pq;
+priority_queue<double> pq;
+vector<double> vec;
 
 int main(){
     cin>>n;
     for(int i=0; i<n; i++){
         double score;
         cin>>score;
-        pq.push(score);
+        if(pq.size()==7){
+            pq.push(score);
+            pq.pop();
+        }else{
+            pq.push(score);
+        }
     }
 
-    int cnt = 0;
     while(pq.size()){
-        cnt++;
-        cout<<fixed<<setprecision(3)<<pq.top()<<"\n";
+        vec.push_back(pq.top());
         pq.pop();
-        if(cnt==7) break;
     }
+    reverse(vec.begin(), vec.end());
+    for(double i: vec) printf("%.3lf\n", i);
+
     return 0;
 }
