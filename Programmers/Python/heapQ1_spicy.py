a66826511a54877b0b30fb45b6e74d3fad52f1f4
mac <mac@macui-noteubug.local>
2024-05-23 23:17:17 +0900

    2024-05-23

--- Programmers/Python/heapQ1_spicy.py
+++ Programmers/Python/heapQ1_spicy.py
@@ -2,20 +2,19 @@ import heapq
 
 def solution(scoville, K):
     answer = 0
-    scoville.sort()
-    while(1):
+    heapq.heapify(scoville)
+    
+    while len(scoville)>1 and scoville[0]<K:
         first = heapq.heappop(scoville)
+        if len(scoville)==0:
+            return -1  
         second = heapq.heappop(scoville)
-        new = first+second*2
+        new = first + second * 2
         heapq.heappush(scoville, new)
-        answer+=1
-        
-        check = True
-        for scovil in scoville:
-            if scovil<K:
-                check = False
-                break
-        if check==True:
-            return answer
+        answer += 1
+    
+    if scoville[0]<K:
+        return -1 
+    return answer
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
