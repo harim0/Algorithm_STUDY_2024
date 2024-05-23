9f300e32e8aa2ed1d60b8b55aac2fcdfb0320c76
mac <mac@macui-noteubug.local>
2024-05-23 14:49:52 +0900

    2024-05-23

--- Programmers/Python/hashQ5_bestAlbum.py
+++ Programmers/Python/hashQ5_bestAlbum.py
@@ -4,18 +4,14 @@ def solution(genres, plays):
     answer = []
     album_list = defaultdict(int)
     
-    for i in range(len(plays)):
-        album_list[genres[i]]+=plays[i]
-    album_list = dict(sorted(album_list.items(), key=lambda items: items[1], reverse=True))
+    for genre, play in zip(genres,plays):
+        album_list[genre]+=play
+    album_list = sorted(album_list.items(), key=lambda items: items[1], reverse=True)
     
-    for album_type in album_list.keys():
-        tmp_album = []
-        for i in range(len(genres)):
-            if album_type==genres[i]:
-                tmp_album.append((plays[i],i))
-        tmp_album.sort()
-        for i in range(2):
-            answer.append(tmp_album.pop()[1])
+    for genre,_  in album_list:
+        tmp_album = [(play, i) for i, (g, play) in enumerate(zip(genres, plays)) if g==genre]
+        tmp_album.sort(key=lambda x: (x[0],-x[1]), reverse=True)
+        answer.extend(idx for _, idx in tmp_album[:2])
     
     return answer
 
