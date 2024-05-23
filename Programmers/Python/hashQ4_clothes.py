c558d8c72c414fef5103413bb18bd06a3145ef88
mac <mac@macui-noteubug.local>
2024-05-23 13:45:39 +0900

    2024-05-23

--- Programmers/Python/hashQ4_clothes.py
+++ Programmers/Python/hashQ4_clothes.py
@@ -20,8 +20,8 @@ def numOfcases(hash_map, k):
     recur_cal(0, 1, 0)
     return result
 
-def solution(clothes):
-    answer = 0
+def solution_f(clothes):
+    answer = 1
     hash_map = {}
     cloth_types = {}
     
@@ -39,7 +39,24 @@ def solution(clothes):
     
     for i in range(len(hash_map),0,-1):
         answer+=numOfcases(hash_map,i)
-        
+    
+    return answer
+
+from collections import defaultdict
+
+def solution(clothes):
+    answer = 1
+    clothes_map = defaultdict(int)
+    
+    if is_empty(clothes):
+        return 0
+    
+    for _, cloth_type in clothes:
+        clothes_map[cloth_type]+=1
+    for cloth_type in clothes_map.keys():
+        answer*=(clothes_map[cloth_type]+1)
+    answer-=1
+    
     return answer
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
