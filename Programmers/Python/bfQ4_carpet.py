1aab7d8bb587fcd62e7a1ba8cd24780917486cf6
mac <mac@macui-noteubug.local>
2024-05-28 19:24:01 +0900

    2024-05-28

--- Programmers/Python/bfQ4_carpet.py
+++ Programmers/Python/bfQ4_carpet.py
@@ -4,6 +4,13 @@ def solution(brown, yellow):
         if hw%h==0 and hw/h+h==(brown+4)/2:
             return sorted([h,hw//h],reverse=True)
 
+# import math # 근의 공식
+# def solution(brown, yellow):
+#     w = ((brown+4)/2 + math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+#     h = ((brown+4)/2 - math.sqrt(((brown+4)/2)**2-4*(brown+yellow)))/2
+#     return [w,h]
+xw
+
 print(solution(10,2))
 print(solution(8,1))
 print(solution(24,24))
