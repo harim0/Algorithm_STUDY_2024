27ca967690f2582aa40c7be0818e85f9d5935baa
mac <mac@macui-noteubug.local>
2024-05-28 15:36:13 +0900

    2024-05-28

--- Programmers/Python/bfQ1_minSquare.py
+++ Programmers/Python/bfQ1_minSquare.py
@@ -1,18 +1,35 @@
-def choices(sizes, newChoices, idx, i):
-    if i>=0:
-        newChoices.append((sizes[i][idx], sizes[i][idx^1]))
-    if i==len(sizes)-1:
-        max_weight = max(item[0] for item in newChoices)
-        max_height = max(item[1] for item in newChoices)
-        # print('newChoices: ',newChoices)
-        # print('max_weight: ',max_weight,', max_height: ',max_height)
-        # print()
-        return max_weight*max_height
-    else:
-        return min(choices(sizes, newChoices[:], 0, i+1), choices(sizes, newChoices[:], 1, i+1))
 
 def solution(sizes):
-    return choices(sizes, [], 0, -1)
+    answer = 1000000
+    while(True):
+        max_zero = (0,0,0)
+        max_one = (0,0,0)
+        for i in range(len(sizes)):
+            if max_zero[0]<sizes[i][0]:
+                max_zero = (sizes[i][0],sizes[i][1],i)
+            if max_one[1]<sizes[i][1]:
+                max_one = (sizes[i][0],sizes[i][1],i)
+        card_size = max_zero[0]*max_one[1]
+        if answer>card_size:
+            answer = card_size
+        if max_zero[0]<=max_one[1] and max_zero[1]<max_zero[0]:
+            sizes[max_zero[2]][0],sizes[max_zero[2]][1]=sizes[max_zero[2]][1],sizes[max_zero[2]][0]
+        elif max_one[1]<max_zero[0] and max_one[0]<max_one[1]:
+            sizes[max_one[2]][0],sizes[max_one[2]][1]=sizes[max_one[2]][1],sizes[max_one[2]][0]
+        else:
+            break
+            
+    return answer
+
+# def solution_gpt(sizes):
+#     max_width = 0
+#     max_height = 0
+    
+#     for width, height in sizes:
+#         max_width = max(max_width, width, height)
+#         max_height = max(max_height, min(width, height))
+        
+#     return max_width * max_height
 
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
 print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
