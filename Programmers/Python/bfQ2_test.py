c67c3f80107616bf670c1b2fdd973ff490f42230
mac <mac@macui-noteubug.local>
2024-05-28 16:53:11 +0900

    ref ++

--- Programmers/Python/bfQ2_test.py
+++ Programmers/Python/bfQ2_test.py
@@ -4,25 +4,20 @@ def solution(answers):
     for std_answer in std_answers:
         std_answer*=((len(answers)//len(std_answer))+1)
     
-    std_corrects = [[0,0],[0,1],[0,2]]
+    std_corrects = [0,0,0]
     for i in range(len(answers)):
         if std_answers[0][i]==answers[i]:
-            std_corrects[0][0]+=1
+            std_corrects[0]+=1
         if std_answers[1][i]==answers[i]:
-            std_corrects[1][0]+=1
+            std_corrects[1]+=1
         if std_answers[2][i]==answers[i]:
-            std_corrects[2][0]+=1
-    std_corrects.sort()
+            std_corrects[2]+=1
     
-    if std_corrects[2][0]==std_corrects[0][0]==std_corrects[1][0]:
-        return [1,2,3]
-    elif std_corrects[2][0]==std_corrects[1][0]:
-        first = std_corrects[2][1]+1
-        second = std_corrects[1][1]+1
-        return sorted([first, second])
-    else:
-        num = std_corrects[2][1]+1
-        return [num]
+    for idx, s in enumerate(std_corrects):
+        if s == max(std_corrects):
+            result.append(idx+1)
+            
+    return result
     
 
 print(solution([1,2,3,4,5]))
