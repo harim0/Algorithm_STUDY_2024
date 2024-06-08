1a81327c5a358fa0803f7335e0f859a3f7b841ea
mac <mac@macui-noteubug.local>
2024-06-08 15:32:12 +0900

    recur

--- Programmers/Python/dpQ1_N.py
+++ Programmers/Python/dpQ1_N.py
@@ -1,7 +1,37 @@
-def solution(N, number):
-    answer = 0
+INF = 9**8
+
+def recur_N(N, number, pnum, k):
+    if pnum==number:
+        return k+1
+    if k>=7:
+        return -1
     
-    return answer
+    # print('pnum : ',pnum, ', k=',k)
+    results = []
+    if pnum!=0:
+        res = recur_N(N, number, pnum*N, k+1)
+        if res!=-1:
+            results.append(res)
+        res = recur_N(N, number, pnum//N, k+1)
+        if res!=-1:
+            results.append(res)
+        res = recur_N(N, number, pnum-N, k+1)
+        if res!=-1:
+            results.append(res)
+    res = recur_N(N, number, pnum+N, k+1)
+    if res!=-1:
+            results.append(res)
+    res = recur_N(N, number, int(str(pnum)+str(N)), k+1)
+    if res!=-1:
+            results.append(res)
+            
+    if not results:
+        return -1
+    else:
+        return min(results)
+
+def solution(N, number):
+    return recur_N(N, number, N, 0)
 
 print(solution(5,12)) # 4
 print(solution(2,11)) # 3
