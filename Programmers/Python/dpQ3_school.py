a9f2343d2883d1e16b93a185a78138293b7b1e3b
mac <mac@macui-noteubug.local>
2024-06-17 16:39:24 +0900

    memoization

--- Programmers/Python/dpQ3_school.py
+++ Programmers/Python/dpQ3_school.py
@@ -1,39 +1,40 @@
 total_calls = 0
 
-def recur_pos(y, x, n, m, roadmap, answer):
+# def recur_pos(y, x, n, m, roadmap, answer):
+#     if x==m-1 and y==n-1:
+#         return 1
+#     paths = 0
+#     if y<n-1 and roadmap[y+1][x]!=0:
+#         paths+=recur_pos(y+1, x, n, m, roadmap, answer)%1000000007
+#     if x<m-1 and roadmap[y][x+1]!=0:
+#         paths+=recur_pos(y, x+1, n, m, roadmap, answer)%1000000007
+#     return paths
+
+def memo_pos(y, x, n, m, roadmap, answer, memo):
+    if x==m-1 and y==n-1:
+        return 1
+    if memo[y][x] != -1:
+        return memo[y][x]
     global total_calls
     total_calls+=1
-    if x==m-1 and y==n-1:
-        print("bingo :: ","(",x,",",y,")")
-        answer+=1
-        return 1
-    if roadmap[y][x]==0:
-        return 0
-    else:
-        print("(",x,",",y,")")
-        paths = 0
-        if y<n-1:
-            paths+=recur_pos(y+1, x, n, m, roadmap, answer)
-        if x<m-1:
-            paths+=recur_pos(y, x+1, n, m, roadmap, answer)
-        return paths
+    paths = 0
+    if y<n-1 and roadmap[y+1][x]!=0:
+        paths+=memo_pos(y+1, x, n, m, roadmap, answer, memo)%1000000007
+    if x<m-1 and roadmap[y][x+1]!=0:
+        paths+=memo_pos(y, x+1, n, m, roadmap, answer, memo)%1000000007
+    memo[y][x] = paths
+    return  memo[y][x]
 
-def re_solution(m, n, puddles):
+
+def solution(m, n, puddles):
     global total_calls
     total_calls = 0
-    roadmap = [[0 if [i+1, j+1] in puddles else -1 for j in range(m)] for i in range(n)]
-    answer = recur_pos(0, 0, n, m, roadmap, 0)
-    print('total_calls = ',total_calls)
-    return answer%1000000007
+    puddles = set((pu[1]-1, pu[0]-1) for pu in puddles)
+    roadmap = [[0 if (i, j) in puddles else -1 for j in range(m)] for i in range(n)]
+    memo = [[-1 for _ in range(m)] for _ in range(n)]
+    answer = memo_pos(0, 0, n, m, roadmap, 0, memo)
+    print('total_calls: ',total_calls)
+    return answer
 
-# print(re_solution(4,3,[[2, 2]]))
-# print(re_solution(7,6,[[2, 2], [6, 6]]))
-
-
-# def solution(m, n, puddles):
-#     dp = [-1]*10001
-#     answer = 0
-    
-#     return answer
-
-# print(solution(4,3,[[2, 2]]))
+print(solution(4,3,[[2, 2]]))
+print(solution(7,6,[[2, 2], [6, 6]]))
