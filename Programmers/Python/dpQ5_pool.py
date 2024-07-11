3003b4367dafcd304362fad15c6e4ea6c17ee6cc
mac <mac@macui-noteubug.local>
2024-07-11 18:26:49 +0900

    runtimeError

--- Programmers/Python/dpQ5_pool.py
+++ Programmers/Python/dpQ5_pool.py
@@ -1,45 +1,26 @@
-def passing(idx, memo):
-    if idx>0:
-        memo[idx-1] = -1
-    if idx<len(memo)-1:
-        memo[idx+1] = -1
-
-def back(idx, memo):
-    if idx>0:
-        if idx-2<0 or (idx-2>=0 and memo[idx-2]!=1):
-            memo[idx-1] = 0
-    if idx<len(memo)-1:
-        if idx+2>len(memo)-1 or (idx+2<=len(memo)-1 and memo[idx+2]!=1):
-            memo[idx+1] = 0
-    
-def re_selection(money, idx, memo, total, k):
-    if idx>=len(money) or memo[idx]==-1 or memo[idx]==1:
+def dp(idx, n, memo, money, first):
+    if idx>=n or (idx==n-1 and first):
         return 0
+    ret = memo[first][idx]
+    if ret!=-1:
+        return ret
+    if idx==0:
+        first = 1
+    ret = max(ret, money[idx]+dp(idx+2, n, memo, money, first))
+    print('1 memo['+str(idx)+']['+str(first)+'] = ',ret)
+    if idx == 0:
+        first = 0
+    ret = max(ret, dp(idx+1, n, memo, money, first))
+    print('\t2 memo['+str(idx)+']['+str(first)+'] = ',ret)
+    memo[first][idx] = ret
+    return ret
 
-    memo[idx] = 1
-    total+=money[idx]
-    passing(idx, memo)
-    print('>> memo: ',memo, ', total: ',total, ', k: ',k)
-    
-    result = total
-    for i in range(idx+2, len(money)):
-        if memo[i]==0:
-            result = max(result, re_selection(money, i, memo, total, k+1))
-            memo[i] = 0
-            back(i, memo)
-        
-    return result
-        
-        
 def solution(money):
     print('problem = ',money)
-    max_sum = 0
-    memo=[0]*len(money)
-    for i in range(len(money)):
-        max_sum = max(max_sum, re_selection(money, i, memo, 0, 0))
-        memo[i] = 0
-        back(i, memo)
-    return max_sum
-    
+    n = len(money)
+    memo = [[-1] * n for _ in range(2)]
+    answ = dp(0, n, memo, money, 0)
+    print('memo = ',memo)
+    return answ
 
 print(solution([1, 2, 3, 1]))
