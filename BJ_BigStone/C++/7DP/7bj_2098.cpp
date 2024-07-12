2529657c599c84390c494bd25c6e028d7ae1841b
mac <mac@macui-noteubug.local>
2024-07-12 20:39:31 +0900

    gpt^^

--- BJ_BigStone/C++/7DP/7bj_2098.cpp
+++ BJ_BigStone/C++/7DP/7bj_2098.cpp
@@ -1,47 +1,40 @@
 #include<iostream>
-#include<vector>
+#include<cstring>
 
 using namespace std;
 
 int w[16][16] = {0,};
-int memo[16] = {0,};
-int rst = 1e9;
+int dt[1<<16][16];
+int n;
 
-void recur_memo(int start, int end, int n, vector<int> bags, int cur_sum){ 
-    if (bags.size()==n-1) {
-        cur_sum+=w[end][0];
-        rst = min(rst, cur_sum);
+int tsp(int curr, int visited){
+    // cout << "visited: " << bitset<16>(visited).to_string().substr(16-n) << " (curr: "<<curr<<")"<<endl;
+    if(visited==(1<<n)-1){
+        cout<<endl;
+        return w[curr][0]?w[curr][0]:1e9;
     }
-
-    memo[end] = 1;
+    if(dt[visited][curr]!=-1){
+        return dt[visited][curr];
+    }
+    int rst = 1e9;
     for(int i=0; i<n; i++){
-        if(i!=start&&i!=end&&memo[i]==0){
-            bags.push_back(w[end][i]);
-            recur_memo(end, i, n, bags, cur_sum+w[end][i]); 
-            bags.pop_back();
+        if(!(visited&(1<<i))&&w[curr][i]!=0){
+            rst = min(rst, w[curr][i]+tsp(i, visited|(1<<i)));
         }
     }
-    memo[end] = 0;
+    dt[visited][curr] = rst;
+    return dt[visited][curr];
 }
 
 int main(){
-    int n;
     cin>>n;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin>>w[i][j];
         }
     }
-
-    vector<int> bags;
-    memo[0] = 1;
-    for(int i=1; i<n; i++){
-        bags.push_back(w[0][i]);
-        recur_memo(0, i, n, bags, w[0][i]);
-        bags.pop_back();
-    }
-
-    cout<<rst<<endl;
+    memset(dt, -1, sizeof(dt));
+    cout<<tsp(0,1)<<endl;
 
     return 0;
 }
