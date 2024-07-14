7ab09e5a8bfd2c217e05c0f82270d75b62048066
mac <mac@macui-noteubug.local>
2024-07-14 20:52:07 +0900

    dp

--- BJ_BigStone/C++/7DP/7bj_17070.cpp
+++ BJ_BigStone/C++/7DP/7bj_17070.cpp
@@ -1,13 +1,42 @@
 #include<iostream>
 #include<vector>
 #include<utility>
+#include <cstring>
 
 using namespace std;
 
 int n;
-int h[16][16]={-1,};
+int h[20][20];
+int dt[20][20][3];
 vector<pair<int, int>> moving = {{0,1},{1,0},{1,1}};
 // int calls = 0;
+int check(int y, int x, int dir){
+    if(y>=n||x>=n) return 0;
+    if(dir==0||dir==1){
+        if(!h[y][x]) return 1;
+    }else if(dir==2){
+        if((!h[y][x])&&h[y-1][x]==0&&h[y][x-1]==0) return 1;
+    }
+    return 0;
+}
+
+int dp_possible(int y, int x, int dir){
+    dt[0][1][0] = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(check(i,j+1,0)) dt[i][j+1][0]+=dt[i][j][0];
+            if(check(i,j+1,0)) dt[i][j+1][0]+=dt[i][j][2];
+
+            if(check(i+1,j,1)) dt[i+1][j][1]+=dt[i][j][1];
+            if(check(i+1,j,1)) dt[i+1][j][1]+=dt[i][j][2];
+
+            if(check(i+1,j+1,2)) dt[i+1][j+1][2]+=dt[i][j][0];
+            if(check(i+1,j+1,2)) dt[i+1][j+1][2]+=dt[i][j][1];
+            if(check(i+1,j+1,2)) dt[i+1][j+1][2]+=dt[i][j][2];
+        }
+    }
+    return dt[n-1][n-1][0]+dt[n-1][n-1][1]+dt[n-1][n-1][2];
+}
 
 int cnt_possible(int y, int x, int dir){
     // calls++;
@@ -31,7 +60,9 @@ int main(){
         for(int j=0; j<n; j++)
             cin>>h[i][j];
 
-    cout<<cnt_possible(0, 1, 0)<<endl;
+    // cout<<cnt_possible(0, 1, 0)<<endl;
+    memset(dt,0,sizeof(dt));
+    cout<<dp_possible(0, 1, 0)<<endl;
     // cout<<calls<<endl;
 
     return 0;
